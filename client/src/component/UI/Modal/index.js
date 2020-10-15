49fb23212cf0397184d7956435b458b1e49d43d4
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-10-15 22:22:11 +0100

    Add Modal Component

---
+++ client/src/component/UI/Modal/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Aux from "react-aux";
+
+import Backdrop from "./Backdrop";
+
+const Modal = (props) => {
+	const backgroundStyle = {
+		backgroundSize: "cover",
+		backgroundImage: `url(https://image.tmdb.org/t/p/original/${
+			props.movie.backdrop_path || props.movie.poster_path
+		})`,
+	};
+
+	return (
+		<Aux>
+			<Backdrop show={props.show} toggleBackdrop={props.modalClosed} />
+			<div style={backgroundStyle} className={props.show ? "modal show" : "modal hide"}>
+				{props.children}
+			</div>
+		</Aux>
+	);
+};
+
+export default Modal;
