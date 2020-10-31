0783c3094ec66428a7d2b0d4217d19adf38f583d
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-10-31 13:53:34 +0100

    connect the whole app with Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,29 @@
 
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promise from "redux-promise";
+import reducers from "./store/reducers";
 import Routing from "./Routing";
+
+import "swiper/swiper-bundle.min.css";
+// import 'swiper/components/navigation/navigation.scss';
+// import 'swiper/components/pagination/pagination.scss';
+// import 'swiper/components/scrollbar/scrollbar.scss';
+
+// Import main sass file to apply global styles
 import "./App.scss";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routing />
-		</BrowserRouter>
+		<Provider store={createStoreWithMiddleware(reducers)}>
+			<BrowserRouter>
+				<Routing />
+			</BrowserRouter>
+		</Provider>
 	);
 }
 
