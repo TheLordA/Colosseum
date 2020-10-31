c1bf1dd23a91264cfb5a458e2d484957d4c28714
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-10-31 13:43:47 +0100

    add NotFound Screen

--- client/src/Routing.js
+++ client/src/Routing.js
@@ -3,19 +3,21 @@ import { Route, Switch } from "react-router-dom";
 import LandingScreen from "./pages/LandingScreen/LandingScreen";
 import LoginScreen from "./pages/LogIn/index";
 import Home from "./pages/Home/index";
+import NotFound from "./pages/NotFound/index";
 
 const Routing = () => {
 	return (
 		<Switch>
-			<Route exact path="/">
+			<Route exact path="/" component={LandingScreen}>
 				<LandingScreen />
 			</Route>
-			<Route exact path="/login">
+			<Route exact path="/login" component={LoginScreen}>
 				<LoginScreen />
 			</Route>
-			<Route exact path="/browse">
+			<Route exact path="/browse" component={Home}>
 				<Home />
 			</Route>
+			<Route component={NotFound} />
 		</Switch>
 	);
 };
