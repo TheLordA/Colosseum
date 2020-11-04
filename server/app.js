53d3283cc30e347d703fb60059c541f7af41ea20
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-11-04 20:43:50 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,9 +2,20 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const path = require("path");
+const passport = require("passport");
+const session = require("express-session");
+const mongoStore = require("connect-mongo")(session);
+const connectDB = require("./config/connectDB");
 
 //Load Config
 dotenv.config({ path: "./config/.env" });
+
+// Passport Configs
+require("./config/localStrategy")(passport);
+
+// Connection to DB
+connectDB();
 
 // Specify the PORT which will the server running on
 const PORT = process.env.PORT || 3001;
@@ -24,10 +35,25 @@ app.use(cors({ credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
+// Sessions
+app.use(
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: false,
+		store: new mongoStore({ mongooseConnection: mongoose.connection }),
+	})
+);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
+app.use("/auth", require("./routes/auth"));
 
 // make the server listen to requests
 app.listen(PORT, () => {
