93f7380b35c5e9575d3fe06232600fa47a348158
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-11-04 20:43:25 +0100

    Create authentification route

---
+++ server/routes/auth.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const { validateLoginInput } = require("../utils/validation");
+const chalk = require("chalk");
+
+// ---------------------- Local Authentication ---------------------- //
+
+// @desc    Auth with Credentials
+// @route   GET /auth/login
+// @access 	Public
+// @test	UnitTest : Verified
+router.post("/login", (req, res, next) => {
+	// Submitted Data Validation
+	const { errors, isValid } = validateLoginInput(req.body);
+	// Check validation
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
+	// CallBack our passportLocal Strategy to authenticate
+	passport.authenticate("local", (err, user, info) => {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			return res.status(401).json({ message: info.message });
+		}
+		res.json({ user });
+	})(req, res, next);
+});
+
+// ------------------------------- END ------------------------------- //
+
+module.exports = router;
