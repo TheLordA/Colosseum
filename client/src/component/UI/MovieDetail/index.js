7c9fdaf730c0773d15bfac8a1564b0d44d38dc17
Anass Ferrak <45141652+TheLordA@users.noreply.github.com>
2021-01-07 21:59:02 +0100

    update Moovie Detail & Modal UI

--- client/src/component/UI/MovieDetail/index.js
+++ client/src/component/UI/MovieDetail/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Aux from "react-aux";
-
-import AddIcon from "../../../assets/images/add-button.svg";
-import PlayIcon from "../../../assets/images/play-button.svg";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import AddIcon from "@material-ui/icons/Add";
 
 const MovieDetails = (props) => {
 	return (
-		<Aux>
+		<>
 			<div className="modal__container">
 				<h1 className="modal__title">{props.movie.title || props.movie.name}</h1>
 				<p className="modal__info">
@@ -14,23 +12,30 @@ const MovieDetails = (props) => {
 					Release date: {props.movie.release_date || props.movie.first_air_date} Runtime:{" "}
 					{props.movie.runtime || props.movie.episode_run_time}m
 				</p>
-				<p className="modal__episode">
-					{props.movie.number_of_episodes
-						? " Episodes: " + props.movie.number_of_episodes
-						: ""}
-					{props.movie.number_of_seasons ? " Seasons: " + props.movie.number_of_seasons : ""}
-				</p>
 				<p className="modal__overview">{props.movie.overview}</p>
-				{/* <button className="modal__btn modal__btn--red">
-					<PlayIcon className="modal__btn--icon" />
-					Play
-				</button>
-				<button className="modal__btn">
-					<AddIcon className="modal__btn--icon" />
-					My List
-				</button> */}
+				{props.movie.media_type === "tv" ? (
+					<p className="modal__episode">
+						{props.movie.number_of_episodes
+							? " Episodes: " + props.movie.number_of_episodes
+							: ""}
+						{props.movie.number_of_seasons
+							? " Seasons: " + props.movie.number_of_seasons
+							: ""}
+					</p>
+				) : null}
+				<div className="button-wrapper">
+					<button className="modal__btn" data-primary={true}>
+						<PlayArrowIcon className="modal__btn--icon" fontSize="large" />
+						Play
+					</button>
+
+					<button className="modal__btn" data-primary={false}>
+						<AddIcon className="modal__btn--icon" />
+						My List
+					</button>
+				</div>
 			</div>
-		</Aux>
+		</>
 	);
 };
 
